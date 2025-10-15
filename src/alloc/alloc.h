388030bf73ca778f654e3c4904c7501087186ebb
DanCpp <danila.ushakov.08@mail.ru>
2025-10-15 11:00:41 +0700

    All down

--- src/alloc/alloc.h
+++ src/alloc/alloc.h
@@ -11,4 +11,10 @@ void* allocate_filled(size_t n, size_t memb);
 void* reallocate(void* old, size_t nmemb);
 void deallocate(void* ptr);
 
+
+#define malloc(_nmemb) allocate(_nmemb);
+#define calloc(n, memb) allocate_filled(n, memb)
+#define realloc(old, nmemb) reallocate(old, nmemb)
+#define free(ptr) deallocate(ptr)
+
 #endif
