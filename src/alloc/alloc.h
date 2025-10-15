93f1f93f5c1421282798fa4014a20501f2cb7980
DanCpp <danila.ushakov.08@mail.ru>
2025-10-15 08:40:14 +0700

    added create and delete functions of allocator and implementation of them.

--- src/alloc/alloc.h
+++ src/alloc/alloc.h
@@ -3,11 +3,8 @@
 
 #include <stddef.h>
 
-struct __allocator__;
-typedef struct __allocator__ allocator;
-
-allocator* create();
-void delete(allocator* alloc);
+void create();
+void delete();
 
 void* allocate(size_t nmemb);
 void* allocate_filled(size_t n, size_t memb);
--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 
+#define USE_STD_ALLOC
+
 #ifdef USE_STD_ALLOC
 #include "std_alloc/std_alloc_wrap.h"
 #else
 #include "alloc/alloc.h"
 #endif
+
+#include <stdlib.h>
+
+
+int main() {
+  #ifndef USE_STD_ALLOC
+  create();
+  atexit(delete);
+  #endif
+}
